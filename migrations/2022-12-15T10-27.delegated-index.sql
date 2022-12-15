d0a64207f49adc3f51ea11e2439c079c7921376a
Stephen Fraser <stephen.fraser@digirati.com>
2022-12-15 10:31:18 +0000

    Fixed delegated index

---
+++ migrations/2022-12-15T10-27.delegated-index.sql
@@ -0,0 +1,3 @@
+--delegated-index (up)
+create index tasks_delegated_task_index
+    on tasks (delegated_task);
---
+++ migrations/down/2022-12-15T10-27.delegated-index.sql
@@ -0,0 +1,2 @@
+--delegated-index (down)
+drop index if exists tasks_delegated_task_index;
