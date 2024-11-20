f19eb1842f8cb0f7419dfb7a816d59012039a33f
Stephen Fraser <stephen.fraser@digirati.com>
2024-11-20 13:41:06 +0000

    Use database pool directly, avoid reserving connections

--- src/middleware/db-connection.ts
+++ src/middleware/db-connection.ts
@@ -4,8 +4,6 @@ import { DatabasePoolType } from 'slonik';
 export const dbConnection =
   (pool: DatabasePoolType): Middleware =>
   async (context, next) => {
-    await pool.connect(async (connection) => {
-      context.connection = connection;
-      await next();
-    });
+    context.connection = pool;
+    await next();
   };
