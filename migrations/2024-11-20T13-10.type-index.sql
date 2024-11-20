1fe6a959f7b953bf7e9e018f6c43ccd5d1682c3f
Stephen Fraser <stephen.fraser@digirati.com>
2024-11-20 15:54:06 +0000

    Added status index

--- migrations/2024-11-20T13-10.type-index.sql
+++ migrations/2024-11-20T13-10.type-index.sql
@@ -4,6 +4,10 @@
 create index tasks_type_index
     on tasks (type);
 
+-- Create index on status column
+create index tasks_status_index
+    on tasks (status);
+
 -- Crete GIN index on context column
 create index tasks_context_gin_index
     on tasks using GIN (context jsonb_ops);
--- migrations/down/2024-11-20T13-10.type-index.sql
+++ migrations/down/2024-11-20T13-10.type-index.sql
@@ -1,3 +1,4 @@
 --type-index (down)
 drop index if exists tasks_type_index;
+drop index if exists tasks_status_index;
 drop index if exists tasks_context_gin_index;
